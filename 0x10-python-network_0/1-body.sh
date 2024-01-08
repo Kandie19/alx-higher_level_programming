b28220e48f3a71d3fdba9c4bb1f4763db6901691
Kandie19 <kandiemasasabi@gmail.com>
2024-01-08 07:31:56 +0000

    Debugging

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,17 +1,3 @@
-L=$1
-
-# Sending GET request and storing the response
-response=$(curl -s -w "%{http_code}" "$URL")
-
-# Extracting the HTTP status code from the response
-status_code=$(echo "$response" | tail -c 3)
-
-# Extracting the body of the response
-body=$(echo "$response" | sed 's/\(.*\)\(....\)$/\1/')
-
-# Display only the body of a 200 status code response
-if [ "$status_code" -eq 200 ]; then
-    echo "Body of the response: $body"
-else
-    echo "Error: HTTP status code $status_code"
-fi
+#!/bin/bash
+# return only 200 status code body
+curl -s -L $1
